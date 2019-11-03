02c6b68b8e715b0fe614076d8d6e96011b9064af
kazukinakamuraKN <kajirikajiri@gmail.com>
2019-11-04 07:35:32 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 directory=_book
+temp_directory=_temp_book
 branch=gh-pages
 build_command() {
     gitbook build
@@ -9,16 +10,21 @@ echo -e "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory
 
 echo -e "\033[0;32mChecking out $branch....\033[0m"
-git worktree add -f $directory $branch
+git worktree add -f $temp_directory $branch
 
 echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
 
+echo -e "\033[0;32mCopy_site...\033[0m"
+cp -r $directory $temp_directory
+
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-cd $directory &&
+cd $temp_directory &&
     git add --all &&
       git commit -m "Deploy updates" &&
         git push origin $branch
 
 echo -e "\033[0;32mCleaning up...\033[0m"
-git worktree prune
+cd ..
+git worktree remove -f $temp_directory
+rm -rf $directory

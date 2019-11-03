cabaa84b219fd13575a73839acd29c4b8e00842e
kazukinakamuraKN <kajirikajiri@gmail.com>
2019-11-04 07:35:06 +0900

    Deploy updates

--- _book/deploy.sh
+++ _book/deploy.sh
@@ -25,5 +25,6 @@ cd $temp_directory &&
         git push origin $branch
 
 echo -e "\033[0;32mCleaning up...\033[0m"
-git worktree remove -f $temp_directory &&\
+cd ..
+git worktree remove -f $temp_directory
 rm -rf $directory
